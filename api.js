eac58b2c47ae544eb4ae9f394cb908df6d1125e3
ferrbaez <baezfer12@gmail.com>
2023-08-09 18:38:29 -0300

    git commit -m
    POST proveedor

--- api.js
+++ api.js
@@ -20,6 +20,8 @@ app.get('/proveedores', (req, res)=>{
     client.end;
 })
 
+//LISTAR PROVEEDOR POR ID
+
 app.get('/proveedores/:id', (req, res)=>{
     client.query(`Select * from proveedores where id=${req.params.id}`, (err, result)=>{
         if(!err){
@@ -29,12 +31,15 @@ app.get('/proveedores/:id', (req, res)=>{
     client.end;
 })
 
+
+// INSERTAR NUEVO PROVEEDOR
+
 app.post('/proveedores', (req, res)=> {
     const proveedores = req.body;
-    console.log('Insert Query:', proveedores);
+    console.log('REQUEST.BODY:', proveedores);
     
     let insertQuery = `INSERT INTO proveedores(nombre, ruc, direccion, ciudad, email, plazoentrega)
-        VALUES( '${proveedores.nombre}', ${proveedores.ruc}, '${proveedores.direccion}', '${proveedores.ciudad}', '${proveedores.email}', '${proveedores.plazoentrega}')`
+        VALUES( '${proveedores[0].nombre}', ${proveedores[0].ruc}, '${proveedores[0].direccion}', '${proveedores[0].ciudad}', '${proveedores[0].email}', '${proveedores[0].plazoentrega}')`
         console.log('Insert Query:', insertQuery); // Add this line for logging
     client.query(insertQuery, (err, result)=>{
         if(!err){
