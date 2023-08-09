6de5926cf13c0506dda0f89f8e36bce364b9a9a5
ferrbaez <baezfer12@gmail.com>
2023-08-09 17:43:54 -0300

    git commit -m
    prueba biblioteca

--- api.js
+++ api.js
@@ -1,6 +1,11 @@
+
 const client = require('./connection.js')
 const express = require('express');
 const app = express();
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
 
 app.listen(3300, ()=>{
     console.log("Sever is now listening at port 3300");
@@ -14,4 +19,32 @@ app.get('/proveedores', (req, res)=>{
     });
     client.end;
 })
+
+app.get('/proveedores/:id', (req, res)=>{
+    client.query(`Select * from proveedores where id=${req.params.id}`, (err, result)=>{
+        if(!err){
+            res.send(result.rows);
+        }
+    });
+    client.end;
+})
+
+app.post('/proveedores', (req, res)=> {
+    const proveedores = req.body;
+    console.log('Insert Query:', proveedores);
+    
+    let insertQuery = `INSERT INTO proveedores(nombre, ruc, direccion, ciudad, email, plazoentrega)
+        VALUES( '${proveedores.nombre}', ${proveedores.ruc}, '${proveedores.direccion}', '${proveedores.ciudad}', '${proveedores.email}', '${proveedores.plazoentrega}')`
+        console.log('Insert Query:', insertQuery); // Add this line for logging
+    client.query(insertQuery, (err, result)=>{
+        if(!err){
+            res.send('Insertion was successful')
+        }
+        else{ console.log(err.message) }
+    })
+    client.end;
+})
+
+
 client.connect();
+
