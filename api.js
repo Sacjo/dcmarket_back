eae991a41ca1dbd4a18cf0bddaa4518e31196bfb
ferrbaez <baezfer12@gmail.com>
2023-08-09 19:45:07 -0300

    fecha
    fecha de registro de proveedores con formato cristiano

--- api.js
+++ api.js
@@ -23,7 +23,7 @@ app.get('/proveedores', (req, res)=>{
 //LISTAR PROVEEDOR POR ID
 
 app.get('/proveedores/:id', (req, res)=>{
-    client.query(`Select * from proveedores where id=${req.params.id}`, (err, result)=>{
+    client.query(`SELECT id, nombre, ruc, direccion, ciudad, email, plazoentrega, to_char(fecharegistro, 'DD-MM-YYYY HH24:MI') AS fecharegistro FROM proveedores where id=${req.params.id}`, (err, result)=>{
         if(!err){
             if (result.rows.length === 0) {
                 res.status(404).send('No encontrado');
@@ -42,8 +42,8 @@ app.post('/proveedores', (req, res)=> {
     const proveedores = req.body;
     console.log('REQUEST.BODY:', proveedores);
     
-    let insertQuery = `INSERT INTO proveedores(nombre, ruc, direccion, ciudad, email, plazoentrega)
-        VALUES( '${proveedores[0].nombre}', ${proveedores[0].ruc}, '${proveedores[0].direccion}', '${proveedores[0].ciudad}', '${proveedores[0].email}', '${proveedores[0].plazoentrega}')`
+    let insertQuery = `INSERT INTO proveedores(nombre, ruc, direccion, ciudad, email, plazoentrega,fecharegistro)
+        VALUES( '${proveedores[0].nombre}', ${proveedores[0].ruc}, '${proveedores[0].direccion}', '${proveedores[0].ciudad}', '${proveedores[0].email}', '${proveedores[0].plazoentrega}', '{${getCurrentTimestamp()}}')`
         console.log('Insert Query:', insertQuery); // Add this line for logging
     client.query(insertQuery, (err, result)=>{
         if(!err){
@@ -98,3 +98,18 @@ app.delete('/proveedores/:id', (req, res)=> {
 
 client.connect();
 
+
+
+
+
+
+function getCurrentTimestamp() {
+    const now = new Date();
+    const day = String(now.getDate()).padStart(2, '0');
+    const month = String(now.getMonth() + 1).padStart(2, '0'); // Months are zero-indexed
+    const year = now.getFullYear();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+
+    return `${day}-${month}-${year} ${hours}:${minutes}`;
+}
