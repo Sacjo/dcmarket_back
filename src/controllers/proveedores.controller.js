9dbd5540e2b41535e56b0d868065b20fa7d2909f
ferrbaez <baezfer12@gmail.com>
2023-08-12 10:55:37 -0300

    Try catch en peticiones

--- src/controllers/proveedores.controller.js
+++ src/controllers/proveedores.controller.js
@@ -1,23 +1,33 @@
 import { Proveedores } from "../models/Proveedores.js";
 
-export const getProveedores = (req, res) => {
-  res.send("getting proveedores");
+//Listar proveedores
+export const getProveedores = async (req, res) => {
+  try {
+    const proveedores = await Proveedores.findAll();
+    res.json(proveedores);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
+
+//Crear un nuevo proveedor
+
 export const createProveedor = async (req, res) => {
-  //Declaramos en constantes los datos que recimibimos en el req.body
-  const { nombre, ruc, direccion, ciudad, email, plazoentrega } = req.body;
+  try {
+    //Declaramos en constantes los datos que recimibimos en el req.body
+    const { nombre, ruc, direccion, ciudad, email, plazoentrega } = req.body;
 
-  const newProveedor = await Proveedores.create({
-    nombre,
-    ruc,
-    direccion,
-    ciudad,
-    email,
-    plazoentrega,
-  });
+    const newProveedor = await Proveedores.create({
+      nombre,
+      ruc,
+      direccion,
+      ciudad,
+      email,
+      plazoentrega,
+    });
 
-  res.send("Proveedor Creado");
-
-  //Ver en consola el objeto que recibimos e insertamos en base de datos
-  console.log(newProveedor);
+    res.json(newProveedor);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
