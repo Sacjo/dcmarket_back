bb7f5bf43ebfdaad3e5332d23980f0e8bf635f8a
ferrbaez <baezfer12@gmail.com>
2023-08-12 16:46:58 -0300

    Idk

--- src/app.js
+++ src/app.js
@@ -2,11 +2,11 @@ import express from "express";
 
 import proveedoresRoutes from "./routes/proveedores.route.js";
 import clientesRoutes from "./routes/clientes.route.js";
-<<<<<<< Updated upstream
+
 import productosRoutes from "./routes/productos.route.js";
-=======
+
 import comprasRoutes from "./routes/compras.route.js";
->>>>>>> Stashed changes
+
 const app = express();
 
 //middlewares
@@ -14,10 +14,9 @@ app.use(express.json());
 
 app.use(proveedoresRoutes);
 app.use(clientesRoutes);
-<<<<<<< Updated upstream
+
 app.use(productosRoutes);
-=======
+
 app.use(comprasRoutes);
->>>>>>> Stashed changes
 
 export default app;
