3281c675913253c358445bc75eb2b2868de95f53
Sacjo <dani.sanchez.13.ds@gmail.com>
2024-02-03 17:45:06 -0300

    variables de entorno

--- src/database/database.js
+++ src/database/database.js
@@ -1,6 +1,10 @@
-import Sequelize from "sequelize";
+import dotenv from 'dotenv';
+dotenv.config();
 
-export const sequelize = new Sequelize("dcmarket", "postgres", "devx", {
-  host: "18.191.33.127",
-  dialect: "postgres",
+import Sequelize from 'sequelize';
+
+export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: 'postgres',
 });
